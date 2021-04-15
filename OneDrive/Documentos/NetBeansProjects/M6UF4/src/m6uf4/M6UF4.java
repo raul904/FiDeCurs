1aeeac837974191444bd374e7ab86141fc7547e8
=Raul gimenez <=raulgimenez904@gmail.com>
2021-04-15 17:51:11 +0200

    fet

--- OneDrive/Documentos/NetBeansProjects/M6UF4/src/m6uf4/M6UF4.java
+++ OneDrive/Documentos/NetBeansProjects/M6UF4/src/m6uf4/M6UF4.java
@@ -18,11 +18,10 @@ public class M6UF4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     //int idProducte, String descripcio, int stockActual, int stockMinim, float pvp
-    
+     //int idProducte, String descripcio, int stockActual, int stockMinim, float pvp   
        Producte producte[] = new Producte[3];
        
-       producte[0] = new Producte(1, "Portable MSI USB 3.0", 7, 3, 50);
+       producte[0] = new Producte(1, "Portable MSI USB 3.0", 9, 3, 50);
        producte[1] = new Producte(2,"Cubo de Rubik", 5,8,25);
        producte[2] = new Producte(3,"Vela", 10,3,1);
        Comanda comanda = new Comanda();
